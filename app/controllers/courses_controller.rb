c63c7c1e8c831b7e44d94b847d92f252147f988f
maimaimai-bit <hmaivu02@gmail.com>
2025-02-25 12:42:01 -0600

    commit

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -15,7 +15,7 @@ class CoursesController < ApplicationController
   def create
     @course = Course.new
     @course.title = params.fetch("query_title")
-    @course.term_offered = params.fetch("query_term_")
+    @course.term_offered = params.fetch("query_term_offered")
     @course.department_id = params.fetch("query_department_id")
 
     if @course.valid?
@@ -27,8 +27,8 @@ class CoursesController < ApplicationController
   end
 
   def update
-    id = params.fetch("path_id")
-    @course = Course.where({ :id => id }).at(0)
+    the_id = params.fetch("path_id")
+    @course = Course.where({ :id => the_id }).at(0)
 
     @course.title = params.fetch("query_title")
     @course.term_offered = params.fetch("query_term_offered")
@@ -43,7 +43,7 @@ class CoursesController < ApplicationController
   end
 
   def destroy
-    the_id = params.fetch("path")
+    the_id = params.fetch("path_id")
     @course = Course.where({ :id => the_id }).at(0)
 
     @course.destroy
